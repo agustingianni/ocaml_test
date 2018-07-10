dd73f2ebd995a1926b0ae7f49bde2a3917ad4798
Agustin Gianni <agustingianni@gmail.com>
2018-07-10 23:39:02 +0200

    Added code to print expressions

--- src/ast.ml
+++ src/ast.ml
@@ -89,13 +89,78 @@ and program =
 and function_parameter =
   | FunctionParameter of qualified_type * string
 
-let expression_to_string expression =
-  match expression with
+let unary_operator_to_string operator =
+  match operator with
+  | PLUS -> "PLUS"
+  | MINUS -> "MINUS"
+  | NOT -> "NOT"
+  | BNOT -> "BNOT"
+
+let binary_operator_to_string operator =
+  match operator with
+  | ADD -> "ADD"
+  | SUB -> "SUB"
+  | MUL -> "MUL"
+  | IDIV -> "IDIV"
+  | FDIV -> "FDIV"
+  | MOD -> "MOD"
+  | AND -> "AND"
+  | OR -> "OR"
+  | XOR -> "XOR"
+  | BAND -> "BAND"
+  | BOR -> "BOR"
+  | BXOR -> "BXOR"
+  | EQ -> "EQ"
+  | NE -> "NE"
+  | LT -> "LT"
+  | GT -> "GT"
+  | LE -> "LE"
+  | GE -> "GE"
+  | ASSIGN -> "ASSIGN"
+  | CONCAT -> "CONCAT"
+  | SHL -> "SHL"
+  | SHR -> "SHR"
+  | IN -> "IN"
+
+let rec expression_to_string expression = match expression with
   | EmptyExpression  -> "EmptyExpression"
-  | UnaryExpression (o, e)  -> "UnaryExpression"
-  | BinaryExpression (o, e0, e1)  -> "BinaryExpression"
-  | FunctionCallExpression (e0, e1) -> "FunctionCallExpression"
-  | IndexExpression (e0, e1) -> "IndexExpression"
-  | FieldAccessExpression (e, n) -> "FieldAccessExpression"
-  | ConstantExpression (c) -> "ConstantExpression"
-  | VariableExpression (i) -> "VariableExpression"
+  | UnaryExpression (o, e)  -> unary_expression_to_string o e
+  | BinaryExpression (o, e0, e1)  -> binary_expression_to_string o e0 e1
+  | FunctionCallExpression (e0, el1) -> function_call_expression_to_string e0 el1
+  | IndexExpression (e0, e1) -> index_expression_to_string e0 e1
+  | FieldAccessExpression (e, n) -> field_access_expression_to_string e n
+  | ConstantExpression (value) -> constant_expression_to_string value
+  | VariableExpression (i) -> variable_expression_to_string i
+
+and unary_expression_to_string operator expression =
+  Printf.sprintf "UnaryExpression { %s %s }"
+    (unary_operator_to_string operator)
+    (expression_to_string expression)
+
+and binary_expression_to_string operator expression0 expression1 =
+  Printf.sprintf "BinaryExpression { %s %s %s }"
+    (binary_operator_to_string operator)
+    (expression_to_string expression0)
+    (expression_to_string expression1)
+
+and function_call_expression_to_string expression0 expression1 =
+  Printf.sprintf "FunctionCallExpression { %s %s }"
+    (expression_to_string expression0)
+    "(expression_to_string expression1)"
+
+and index_expression_to_string expression0 expression1 =
+  Printf.sprintf "IndexExpression { %s %s }"
+    (expression_to_string expression0)
+    (expression_to_string expression1)
+
+and field_access_expression_to_string expression field_name =
+  Printf.sprintf "FieldAccessExpression { %s %s }"
+    (expression_to_string expression)
+    field_name
+
+and constant_expression_to_string expression = match expression with
+  | BooleanValue (value) -> Printf.sprintf "BooleanValue { %b }" value
+  | IntegerValue (value) -> Printf.sprintf "IntegerValue { %u }" value
+
+and variable_expression_to_string identifier = match identifier with
+  | Identifier(name) ->  Printf.sprintf "VariableExpression { %s }" name
